ade810c3d052e2374613582907bf445418a53966
kwoneunchong <eunchong14937@gmail.com>
2021-07-23 18:58:15 +0900

    0723

--- js/common.js
+++ js/common.js
@@ -2,4 +2,19 @@ $(function() {
     $('#sub').hide();
     $('#main_nav').hover(function(){
         $(this).parent().find('#sub').slideDown();
-        
+        $(this).parent().hover(function(){
+
+        },function() {
+            $(this).parent().find('#sub').slideUp(900);
+        })
+    });
+});
+
+$(function(){
+    $('.sub').hide();
+    $('.menu li').on('mouseover',function(){
+        $('.sub',this).slideDown('slow');
+    }).on('mouseout',function(){
+        $('.sub',this).slideUp('fast');
+    });
+});
