6b5860cf2fa15bd1d762eb6ab4b17b3de79e6360
kwoneunchong <eunchong14937@gmail.com>
2021-08-06 16:36:51 +0900

    0806

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
-const subs = document.getElementsByClassName("pick")
+// const subs = document.getElementById("pickbtn");
+// const subs = document.getElementById('slideIn').getElementsByClassName('pick');
 
-subs.addEventListener("click", function() {
-    if(subs.innerText === '♡') {
-        subs.innerText = '♥';
-    } else subs.innerText ='♡';
+// subs.addEventListener("click", function() {
+//     if(subs.innerText === '♡') {
+//         subs.innerText = '♥';
+//     } else subs.innerText ='♡';
+// });
+
+
+
+$(".pick").click(function(){ 
+    var like = $(this).text();
+
+    if (like === '♡') {
+        $(this).text('♥');
+    } else {
+        $(this).text('♡');
+    }
+    // if(subs.innerText === '♡') {
+    //     subs.innerText = '♥';
+    // } else subs.innerText ='♡';
 });
